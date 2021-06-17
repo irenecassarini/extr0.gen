e4a65dc46fdee503012abe1d68d57daa434c4af9
irenecassarini <47857296+irenecassarini@users.noreply.github.com>
2021-06-17 19:03:26 +0200

    up

--- src/index.js
+++ src/index.js
@@ -1,54 +1,54 @@
-var tracklist1 = "MarieRoseLAB - A3, <br> Giek_1 - Ascending <br>  Turian - Sacrifice <br>  M.E.S.H. - Kalimbopolis <br> Nicolas Gaunin - Vavau <br>  Nyakinyua - Rise <br> Varg - (+46) Placing My IPhone X Facing Up To See When U Answer My Texts <br> Toxe - Let Me Thru <br>  WULFFLUW XICV - Cachaos ft Santa Muerte <br> Drippin - Waterfall<br> Air Max ’97 - Hounded<br>M.E.S.H. - Damaged merc<br> Laurel Halo & Hodge - Tru <br> Djrum - Untitled 9<br>  Sumorai - Unreal"
-var tracklist2 = "Hank Williams - Lost Highway <br> Hecq - IV <br> David Eugene <br> Edwards and Alexander Hacke - Lily <br> Body Sculptures - On the Flowers Face <br> The Bug & Earth - American Dream <br> Surachai - An Abandoned Throne in the Hall of Extinction <br> Dirty Beaches - Lone Runner <br> Ben Frost - Sleeping Beauty <br> Rick de Greef - Redgld black <br> Rick de Greef - Hersenmist <br> Rick de Greef - Zand <br> Rick de Greef - Outro (Uit de tijd gekomen) <br> Emptyset - Function: Vulgar Display of Power (Roly Porter Variation) <br> asmsorod - forbidden daughter of god <br> VHK - Nincsen távol <br> Lustmord - Andras Sodom <br> Hoyt axton - Blue Prelude"
-var tracklist3 = "333coconutz - a thin paper sleeve where my pencil dance as tears fall on your body <br> SSIEGE - Nuovo eden [unreleased] <br> SSIEGE - Hold you [unreleased] <br> jjjacob - Particulates <br> Peder Mannerfelt - Perspectives <br> Second Woman - I E/P <br> Kowton - Pea Soup <br> Lēvo - Mbombo <br> Lokane - In the trap Nervous Horizon <br> Air Max 97 - Fruit Crush <br> Habib1 - QUINCENERA <br> Dehousy - Gates <br> K A D A H N / E L A D A I R - New Vision <br> Bonfire <br> NKISI - Kill <br> Ziùr - Cipher <br> Lorenzo Senni - One life, One chance"
+var bio1 = "Guenter Råler is a producer/DJ inspired by experimental club, grime and trance. Her style is eclectic and unpredictable, always mixing unconventionally."
+var bio2 = "Sukubratz bio here!"
+var bio3 = "Insan's Bio here!"
 var tracklist4 = "Réelle - 11.okt.2018 <br> Arca - EN <br>Arca - Sad Bitch <br> Sissel Wincent - Still Undetermined  <br> Weightausend - Seq Blood <br>Katatonic Silentio - Fire <br>Shcuro & ViL - Fuck <br>Elysia Crampton - Oscollo (drums only version) <br> Murlo - Furnace <br>Cryptolect - Punished <br> LUKANN - Home <br> CORIN - Elevate <br>Voiron - Hardchore <br>SOPHIE - Reason Why ft. Kim Petras <br>SHODAN - TEARS <br> Gabber Modus Operandi - Sangkakala III <br> Kamixlo - Mi Sabor (Estoc remix) <br> Laura Grabb - Ion Spot <br> HAJ300 - lat mig <br> Lotic - Damsel in Distress (excerpt)"
 var tracklist5 = "DJ Logic ft. DJ plead - NA <br>Geeneus ft. Katy B - As I <br>Dinamarca - Nicole <br>DJ Logic - Precision <br>Kelela - Turn to dust (leonce remix) <br>Husn hai suhana (Coolie No.1) <br>Emo kid - Enkwarini <br>Sho madjozi - John cena <br>DOT - Ubiza wethu <br>Jumping back slash - Untitled darknezz <br>Cazeria cazador - En la noche estas sol@ <br>Ahadadream - Rosalia drum dub <br>Habibi boi - Amizade falsa <br>Lawd knows - Fantasy bootleg <br>Fatima al qadiri - galby <br>Vanyfox - Good day <br>Fatty fever - Hermy <br>Danifox - Para voce tania <br>SHE spells doom - Moments remix (S.S.D. passion fruit mix)"
 var tracklist6 = "Sara Davachi - Buhrstone x Frank Ocean Godspeed blend <br>Ana Caprix x Kamixlo - Como Si Fuera Tu Ultimo<br>Thugwidow - Jesus H Fucking H Christ<br>Max Dahlhaus - MUTATIONS IN TIME<br>Loefah - midnight<br>blastah - D Major XO life<br>Kelela - A Breath Away (Acapella)<br>Dirty K - Ego Fusion (feat Chams)<br>Tension - Cargo<br>Astrosuka - NDS<br>Sophie - Immaterial<br>Funeral - Asfixia<br>Fran G - Destemperado<br>Born in Flamez - Body to mind edit<br>Arca - Urchin<br>SKY H1 & Ssaliva - Dooms x Rosalìa Bagdad Accapella blend<br>Kelela - AQUAPHORIA<br>Lanark Artefax - Touch Absence"
 var tracklist7 = "Solange - Beltway <br>King Krule - Out Getting Ribs (Space Cadet Remix) <br>Frank Ocean - Little Demon (Arca Remix) <br> Max Cooper - Repetition <br> Classica - Summer Of Haze <br> TMRW - Deathtrap<br> No Translation - To Resume <br> Duni & FRANCE '98 - Te Ha <br> sale gosse lacoste - j'suis ton boy - ft mia ox <br>Time Warp Device - data drain <br> Brujas (HABIBIBOI VIP_Bami Brujero Remix) <br> M¥SS KETA - IN GABBIA (NON CI VADO) <br> DJ Nigga Fox - LUMI <br> Dj Plead - Kirlis <br> Abssys - Chambiao <br> VFO89 -- GlCtçFnK_2651 <br> Golden - Epifanía (PRE FINAL1) <br> ALonzo - Up All Nigt <br> DYNE - REALLY HOT BAILE <br> Yre Den - Oblong Ark <br> Buraka Som Sistema - Hypnotized <br> Princess Nokia - Tomboy"
 var tracklist8 = "KhalilH2OP - Sectioned <br>Concrete fantasies - search and rescue (eternal drift)<br>ssaliva - jazz <br>RIVAL CONSOLES - UNTRAVEL RMX (ONOCLEA SENSIBILIS)<br>LSDXOXO - Black Widow ft. BbyMutha<br>ASJ - Desolation 寂<br>De Grandi - Laguna<br>DJ Plead - Massari and Me<br>Dirty K - Pyroclasm<br>Dj Polo - Date Night at BK<br>Sega Bodega - Nivea<br> EYCEE -Delicate Drippin<br>NOIRE - TWO FUQS<br>Aisha Devi - I'm Not Always Where My Body Is (Varg Remix)<br>ava - well fed<br>Ghoul Boi - Real Fake (unreleased)<br>Clara! y Maoupa - Ruge<br>DECAP × Imaabs - Sofoca Bounca（kojikoji Mushup）"
-var tracklist9 ="Sad Girl Club - Ariel Zetina <br> Harvest Charm - Doline <br>Waxwing Air - Huerta <br> Space Apples - Addison Groove <br> UFO (Who R U?) - Renaissance Man <br> Time (feat Parris) - Wen <br> Drinking Coffee Everyday - Bergsonist <br> Forth - Will Lister <br>Diamond Dub - Calibre <br> Pippin - Perko"
+var tracklist9 = "Sad Girl Club - Ariel Zetina <br> Harvest Charm - Doline <br>Waxwing Air - Huerta <br> Space Apples - Addison Groove <br> UFO (Who R U?) - Renaissance Man <br> Time (feat Parris) - Wen <br> Drinking Coffee Everyday - Bergsonist <br> Forth - Will Lister <br>Diamond Dub - Calibre <br> Pippin - Perko"
 
 var tracklistm1 = "Blood Orange - By Ourselves <br> A$AP Rocky - Purity (Audio) ft. Frank Ocean <br> Yves Tumor - Limerence (Crackberry remix) <br> Yushh - Intense Leg Connection <br> Forever - i-35 <br> CHLO - Ven a mi <br> Kouslin - Sharper <br> Syn - Behemoth <br> SKY H1 - I think I am <br> Inef Coupe - Oil + Water <br> Lorenzo Senni - Dance Tonight Revolution Tomorrow <br> séverine - HEAVENSGATE <br> Hypna - Infrared <br> séverine - CASH CIRCUIT (MEGAN THEE STALLION X M.E.S.H.) <br> Debby Friday - STAY UP <br> Zebra Katz - NO 1 ELSE <br> Arca - Front Load <br> A.G. Cook - Idyll"
-var tracklistm2 ="Shushu - So Sick of Lif3 <br> Oblinof - Holotropic (Evil Medvěd Rmx) <br>Xr/Spool - Us Rwht <br> DJ Asbestos - kiss it better (euphoric Glossss mix) <br> GRRL VS 大悲咒 - Impact Mantra (Tzusing Glued) <br> April Bourne & Ezra V - Dike Children <br> Oklou - just level 5 cause its cute <br> Ljj555998 - I'm in love with the slut <br> TSVI - Antares <br> Comastasis - Stagnant <br> Rapala700 - bloto <br> Charli XCX - pink diamond <br> Lou Kessler - Geotraumatics <br> Avbvrn - Residue <br> A.G Cook - XXoplex <br> Stasya - SAVE ROOM FOR 2 <br> Enei - Hot Plate"
+var tracklistm2 = "Shushu - So Sick of Lif3 <br> Oblinof - Holotropic (Evil Medvěd Rmx) <br>Xr/Spool - Us Rwht <br> DJ Asbestos - kiss it better (euphoric Glossss mix) <br> GRRL VS 大悲咒 - Impact Mantra (Tzusing Glued) <br> April Bourne & Ezra V - Dike Children <br> Oklou - just level 5 cause its cute <br> Ljj555998 - I'm in love with the slut <br> TSVI - Antares <br> Comastasis - Stagnant <br> Rapala700 - bloto <br> Charli XCX - pink diamond <br> Lou Kessler - Geotraumatics <br> Avbvrn - Residue <br> A.G Cook - XXoplex <br> Stasya - SAVE ROOM FOR 2 <br> Enei - Hot Plate"
 var tracklistm3 = "Mica Levi - Interlude 1 <br>ssaliva - jazz <br>IVVVO - Last Days Ft Maxwell Sterling <br>Luxxuryproblems - environments (variations) <br>ssaliva - shard <br>Guenter Råler - No ending <br>silént phil, SIQMENT.HIDDEN, DEAD XICE, data drain, gurtle - Enter The Facility <br>Precolumbian - MORPHINE X RESPIRATOR <br>silént phil - immortal <br>since2side - Quell <br>Beham - Spiders <br>De Grandi - Mercé La Zone <br>Aïsha Devi - I'm Not Always Where My Body Is (Varg Remix) <br>7777 の天使 - I Felt The Golden Gates <br>Frank Ocean - Rushes <br>Solange - Beltway <br>Sega Bodega & Eartheater - Fade Into You <br>YvesTumor - Lifetime"
-var tracklistm4="Cedric Madden - Racer <br>Oklou - Holy Lost Arizona <br>Vince Staples (Feat. Yugen Blakrok And Kendrick Lamar) - Opps  <br>Chynna - Attention <br>Dirty K - Torment<br>Odete - TO THOSE WHO ARE PARANOID, BROKEN, BORED 7  <br>Dreams - Terraform <br>Microhm - Kyoke <br>Nick Leon - Grillo <br> Europa - Alors on dorme <br>Raiden - Bulma feat. Nahshi (Turian Remix) <br>séverine - LOOK HOW HARD IM INTO YOU <br>turian - exo <br>Lamb Kebab & Doll Face - Burn <br> Alignment - 1992 <br> Miss Jay - Homance or Bromance <br> Siu Mata & Amor Satyr - Neutrino <br> Merca Bae & Miss Jay - Portal <br>J-Zbel - Hardcore Jusqu'au PEF <br>DEAD XICE - SSS <br>Bamba Pana - Kusini (Slikback remix)"
+var tracklistm4 = "Cedric Madden - Racer <br>Oklou - Holy Lost Arizona <br>Vince Staples (Feat. Yugen Blakrok And Kendrick Lamar) - Opps  <br>Chynna - Attention <br>Dirty K - Torment<br>Odete - TO THOSE WHO ARE PARANOID, BROKEN, BORED 7  <br>Dreams - Terraform <br>Microhm - Kyoke <br>Nick Leon - Grillo <br> Europa - Alors on dorme <br>Raiden - Bulma feat. Nahshi (Turian Remix) <br>séverine - LOOK HOW HARD IM INTO YOU <br>turian - exo <br>Lamb Kebab & Doll Face - Burn <br> Alignment - 1992 <br> Miss Jay - Homance or Bromance <br> Siu Mata & Amor Satyr - Neutrino <br> Merca Bae & Miss Jay - Portal <br>J-Zbel - Hardcore Jusqu'au PEF <br>DEAD XICE - SSS <br>Bamba Pana - Kusini (Slikback remix)"
 var tracklistm5 = "Lanark Artefax - Voices Near The Hypocentre <br>Doubt - HD Tool<br>Lo Shea - Prang<br>Giant Swan - IGOM<br>Ausschuss- Def Recco <br>Know V.A. - Til The Sun Rises <br>Know V.A. - Somato<br>F͇a̹u̼s̚t͌o̲ M̤e̝r͗̍͊cͥi͎e̵r̶ - MIND BUSINESS (Ice_Eyes Remix) <br>E-Saggila - Tilt<br>De Grandi - My Volvo, My Rules (-555- Mix)<br>Funeral - 01011000<br>Anetha - Disinhibition<br>Blawan - Many many pings<br>West Norwood Cassette Library - Drop (Knowing Looks Remix) <br>KESMO - Swip Hard<br>L O S C I - On Both Sides of the Rifle<br>LADONNA ZYLBERSZTAJN - WORMS<br>DJ Dione - 1 Beat 2 Heaven"
 var tracklistm6 = "Thirdworldlabour - Totem (The Revolution Will Not Be Streamed) <br> Oklou - God's chariots <br>jjjacob - Cestoda <br>since2side - Quell <br>Kris Vango - Theta [featuring Lyra Pramuk] <br>Christoff Riedel - Aere <br>Shayu - Blingy <br>111X - Shiver <br>WULFFLUW XCIV - Night Romance Or Story About Dancefloor Heartbreak <br>RO Zar - Growth <br> KMRU - eve_rem <br> MRTRY - MUTATE SHELL <br> KABLAM - I Am Baby <br> Nahash - The Horns feat. Osheyack <br> Know V.A. - Mirrorscreen <br> TCV - CIU <br> MOESHA13 - No Go Zone"
 
 
-function handleManifesto(){
-   window.location = 'manifesto.html';
+function handleManifesto() {
+  window.location = 'manifesto.html';
 }
 
-function handleAbout(){
-   window.location = 'about.html';
+function handleAbout() {
+  window.location = 'about.html';
 }
 
-function handleLiveShows(){
-   window.location = 'liveshows.html';
+function handleLiveShows() {
+  window.location = 'liveshows.html';
 }
 
-function handleHome(){
-   window.location = 'home.html';
+function handleHome() {
+  window.location = 'home.html';
 }
 
-function handleMembers(){
-   window.location = 'members.html';
+function handleMembers() {
+  window.location = 'members.html';
 }
-function BuyMobileEnergy(){
-   window.open("https://guenterraler.bandcamp.com/album/mobile-energy-ep");
-}
-
-function BuyStNicolai(){
-   window.open("https://kalkatrazcassettes.bandcamp.com/album/st-nicolai");
+function BuyMobileEnergy() {
+  window.open("https://guenterraler.bandcamp.com/album/mobile-energy-ep");
 }
 
-function BuyUBS(){
-    window.open("https://guenterraler.bandcamp.com/album/ugly-bathroom-selfies");
+function BuyStNicolai() {
+  window.open("https://kalkatrazcassettes.bandcamp.com/album/st-nicolai");
 }
 
-function BuyKaneda(){
-    window.open("https://guenterraler.bandcamp.com/album/kaneda-series");
+function BuyUBS() {
+  window.open("https://guenterraler.bandcamp.com/album/ugly-bathroom-selfies");
+}
+
+function BuyKaneda() {
+  window.open("https://guenterraler.bandcamp.com/album/kaneda-series");
 }
 
 
@@ -107,21 +107,21 @@ function myFunction9() {
 
 
 
-function handleMix1() {
+function handleBio1() {
   var popup = document.getElementById("m1");
   popup.classList.toggle("show");
-  document.querySelector("#m1").innerHTML = `${tracklistm1}`
+  document.querySelector("#m1").innerHTML = `${bio1}`
 }
 
-function handleMix2() {
+function handleBio2() {
   var popup = document.getElementById("m2");
   popup.classList.toggle("show");
-  document.querySelector("#m2").innerHTML = `${tracklistm2}`
+  document.querySelector("#m2").innerHTML = `${bio2}`
 }
-function handleMix3() {
+function handleBio3() {
   var popup = document.getElementById("m3");
   popup.classList.toggle("show");
-  document.querySelector("#m3").innerHTML = `${tracklistm3}`
+  document.querySelector("#m3").innerHTML = `${bio3}`
 }
 function handleMix4() {
   var popup = document.getElementById("m4");
@@ -139,7 +139,7 @@ function handleMix6() {
   document.querySelector("#m6").innerHTML = `${tracklistm6}`
 }
 
-function handleFollow(){
+function handleFollow() {
   window.open("https://www.instagram.com/extr0.gen/");
 }
 
